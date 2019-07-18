22786913a962800a92ef39be604f79bfeb39f870
Flying Katsu <Warped2713@users.noreply.github.com>
2019-07-17 21:19:09 -0400

    add gitignore, sample secret file, output dir

---
+++ secret.sample.py
@@ -0,0 +1,5 @@
+import os
+os.environ['TwitterReplyAnalyzer_ConsumerKey'] = ''
+os.environ['TwitterReplyAnalyzer_ConsumerSecret'] = ''
+os.environ['TwitterReplyAnalyzer_AccessToken'] = ''
+os.environ['TwitterReplyAnalyzer_AccessSecret'] = ''
